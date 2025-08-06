1f0c379e5e322f7b09bf039bc6b9353ad9c18c34
Jatin Sharma <jatinsharma9038@gmail.com>
2025-08-06 13:09:08 +0530

    FIX: Fixed the login Page bug.

--- frontend/src/Pages/LogIn.jsx
+++ frontend/src/Pages/LogIn.jsx
@@ -20,7 +20,7 @@ const LogIn = () => {
                 userObject[item.name] = item.value;
             }
         }
-        
+
         if (email && password) {
             Axios(URL, {
                 method: "POST",
@@ -60,50 +60,50 @@ const LogIn = () => {
         <div className="h-full flex flex-col justify-evenly items-center w-2/5 mx-auto  shadow-md px-10 py-8 bg-gray-50 rounded-md">
             <div className="flex flex-col items-center">
                 <img src="/Design.png" className="w-20" alt="Logo" />
-                <h3 className="mt-2 text-lg font-medium san">
-                    Remote Executor
-                </h3>
+                <h3 className="mt-2 text-lg font-medium san">Remote Executor</h3>
             </div>
-            <form onSubmit={handleSubmit} className="flex flex-col w-full my-2">
-                <span>
-                    <input
-                        type="email"
-                        name="email"
-                        value={email}
-                        placeholder="E-mail address"
-                        className="w-full my-2 py-3 px-4 rounded-md font-extralight border-x border-y"
-                        onChange={(e) => {
-                            setEmail(e.target.value);
-                        }}
-                    />
-                </span>
-                <span>
-                    <input
-                        type="password"
-                        name="password"
-                        value={password}
-                        placeholder="Password"
-                        className="w-full my-2 py-3 px-4 rounded-md font-extralight border-x border-y"
-                        onChange={(e) => {
-                            setPassword(e.target.value);
-                        }}
-                    />
-                </span>
-                <span>
-                    <button className="w-full my-2 text-white bg-gradient-to-bl from-slate-500 to-slate-800 py-3 px-4 rounded-md">
-                        Sign In
-                    </button>
-                </span>
-            </form>
-            <div>
-                <p className="text-sm text-gray-400 font-light">
-                    New user?
-                    <Link to="/signup" className="text-blue-900 text-base ml-1">
-                        Sign Up
-                    </Link>
-                </p>
-            </div>
-            {token && (
+
+            {!token ? (
+                <form onSubmit={handleSubmit} className="flex flex-col w-full my-2">
+                    <span>
+                        <input
+                            type="email"
+                            name="email"
+                            value={email}
+                            placeholder="E-mail address"
+                            className="w-full my-2 py-3 px-4 rounded-md font-extralight border-x border-y"
+                            onChange={(e) => {
+                                setEmail(e.target.value);
+                            }}
+                        />
+                    </span>
+                    <span>
+                        <input
+                            type="password"
+                            name="password"
+                            value={password}
+                            placeholder="Password"
+                            className="w-full my-2 py-3 px-4 rounded-md font-extralight border-x border-y"
+                            onChange={(e) => {
+                                setPassword(e.target.value);
+                            }}
+                        />
+                    </span>
+                    <span>
+                        <button className="w-full my-2 text-white bg-gradient-to-bl from-slate-500 to-slate-800 py-3 px-4 rounded-md">
+                            Sign In
+                        </button>
+                    </span>
+                    <div>
+                        <p className="text-sm text-gray-400 font-light">
+                            New user?
+                            <Link to="/signup" className="text-blue-900 text-base ml-1">
+                                Sign Up
+                            </Link>
+                        </p>
+                    </div>
+                </form>
+            ) : (
                 <span>
                     <button
                         className="w-full my-2 text-white bg-gradient-to-bl from-slate-500 to-slate-800 py-3 px-4 rounded-md"
